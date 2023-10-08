9a9613e910f135b4c76dffba38d0767a25f7dbd1
Carlos Hernandez <chernandezmiranda@gmail.com>
2023-10-08 10:35:06 -0700

    updated walking

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -23,29 +23,60 @@ function newNonPlayableCharacter(x, y) {
 
     setInterval(moveCharacter, 1)
 
-    function walkEast() {
+    function walkEast(time) {
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
+        return new Promise(resolve => {
+            setTimeout(() => {
+                stop()
+                resolve()
+        }, time)
+        })
     }
+    
 
-    function walkNorth() {
+    function walkNorth(time) {
         direction = 'north'
         element.src = `./assets/red-character/north.gif`
+        return new Promise(resolve => {
+            setTimeout(() => {
+                stop()
+                resolve()
+        }, time)
+        })
     }
 
-    function walkWest() {
+    function walkWest(time) {
         direction = 'west'
         element.src = `./assets/red-character/west.gif`
+        return new Promise(resolve => {
+            setTimeout(() => {
+                stop()
+                resolve()
+        }, time)
+        })
     }
 
-    function walkSouth() {
+    function walkSouth(time) {
         direction = 'south'
         element.src = `./assets/red-character/south.gif`
+        return new Promise(resolve => {
+            setTimeout(() => {
+                stop()
+                resolve()
+        }, time)
+        })
     }
 
-    function stop() {
+    function stop(time) {
         direction = null
         element.src = `./assets/red-character/static.gif`
+        return new Promise(resolve => {
+            setTimeout(() => {
+                stop()
+                resolve()
+        }, time)
+        })
     }
 
     return {
@@ -56,4 +87,10 @@ function newNonPlayableCharacter(x, y) {
         walkSouth: walkSouth,
         stop: stop
     }
+}
+
+function sleep(time){
+    return new Promise(resolve => {
+        setTimeout(resolve, time)
+    })
 }
